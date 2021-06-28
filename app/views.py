7f3bd9a99112183d8473847f93e4aeb736b78682
Ebraheem Abdulrazzaq <81966158+Ebraheem-mis@users.noreply.github.com>
2021-06-28 17:29:08 +0000

    Services list view function

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Service
 # Create your views here.
+
+
+
+def services_list_view(request):
+  data={}
+  service_list = Service.objects.all()
+  data['Service'] = service_list
+  return render(request, "service_list_view.html", context=data)
